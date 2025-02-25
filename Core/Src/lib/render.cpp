bfb62ae9c3bb4d1e9fb5c53130a235e5bc71b187
jeremymacchiarella <133280071+jeremymacchiarella@users.noreply.github.com>
2025-02-24 20:28:04 -0800

    added drawing x and drawing grid
    added functions to draw an x, draw the grid, and helper functions to draw line and draw diagonal line

--- Core/Src/lib/render.cpp
+++ Core/Src/lib/render.cpp
@@ -22,6 +22,30 @@ void Render::LCDRenderer::drawRect(uint16_t x, uint16_t y, uint16_t width, uint1
   }
 }
 
+void Render::LCDRenderer::drawDiagonalLine(uint16_t x, uint16_t y, uint16_t size, bool topLeftToBottomRight, Render::LCDColor color) {
+    if (topLeftToBottomRight) {
+        //you can change if you want to draw diagonal going right or left with this bool
+        for (uint16_t i = 0; i < size; ++i) {
+            lcd->drawRect(x + i, y + i, 1, 1, color.as565());  // Draw each pixel along the diagonal individually
+        }
+    } else {
+        // Draw top-right to bottom-left diagonal
+        for (uint16_t i = 0; i < size; ++i) {
+            lcd->drawRect(x + size - i - 1, y + i, 1, 1, color.as565());  // Draw each pixel along the diagonal individually
+        }
+    }
+}
+
+void Render::LCDRenderer::drawX(uint16_t x, uint16_t y, uint16_t size, Render::LCDColor color) {
+    // draw first diagonal left to right
+    drawDiagonalLine(x, y, size, true, color);
+
+    // draw second diagonal right to left
+    drawDiagonalLine(x + size - 1, y, size, false, color);
+}
+
+
+
 /*
     TODO: Could possibly split this into a cleaner rounded rectangle corner fn.
     Then just use that 4 times with a certain fidelity.
@@ -50,6 +74,36 @@ void drawSingleCircleOutline(Display::Controller* lcd, uint16_t x, uint16_t y, u
         x0++;
     }
 }
+
+void Render::LCDRenderer::drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color) { //this function only works for horizontal and vertical lines
+    //horizontal lines
+    if (y0 == y1) {
+        for (uint16_t x = x0; x <= x1; x++) {
+            lcd->drawRect(x, y0, 1, 1, color);  // Draw each pixel in the horizontal line
+        }
+    }
+    // vertical line
+    else if (x0 == x1) {
+        for (uint16_t y = y0; y <= y1; y++) {
+            lcd->drawRect(x0, y, 1, 1, color);  // Draw each pixel in the vertical line
+        }
+    }
+}
+
+
+void Render::LCDRenderer::drawGrid() { //maybe can change this to draw grid a bit differently if it looks weird
+    
+    //draw horizontal lines
+    drawLine(0, 80, 320, 80, currentColor.as565());
+    drawLine(0, 160, 320, 160, currentColor.as565());
+
+
+
+    // draw vertical lines, wont be exactly symmetrical because it is 320 pixels in x direction which is not divisible by 3 but likely wont be noticable
+    drawLine(107, 0, 107, 240, currentColor.as565());
+    drawLine(214, 0, 214, 240. currentColor.as565());
+}
+
 
 void Render::LCDRenderer::drawCircleOutline(uint16_t x, uint16_t y, uint16_t radius, uint8_t thickness) {
     for (uint8_t i = 0; i < thickness; i++) {
